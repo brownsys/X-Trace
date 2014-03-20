cd59a3ad84a11061abbb2410126a5e683aec1386
Jonathan Mace <jonathan.c.mace@gmail.com>
2014-03-20 10:57:08 -0700

    Modified available API methods for creating new contexts

--- src/main/java/edu/brown/cs/systems/xtrace/Context.java
+++ src/main/java/edu/brown/cs/systems/xtrace/Context.java
@@ -25,48 +25,40 @@ public class Context {
   }
   
   /**
-   * Creates a new context that has neither a taskid, nor a tenantclass, nor any parent ids
+   * Creates and returns a new Context.  The context is not set anywhere; call a set(Context)
+   * method to set it to the active context.
+   * @param randomTaskID if true, generates and includes a random task ID in the metadata. if false, no task id is included
+   * @param isCausal if randomTaskID is true and isCausal is true, adds causality tracing to the metadata
+   * @param tenantClass if null, no tenant class is included. if not null, the tenantclass specified is included
+   * @return a new context
    */
-  public static Context blank() {
-    return new Context(XTraceMetadata.newBuilder());
+  public static Context create(boolean randomTaskID, boolean isCausal, Integer tenantClass) {
+    if (randomTaskID && isCausal)
+      return create(Logger.random.nextLong(), tenantClass, 0L);
+    else if (randomTaskID)
+      return create(Logger.random.nextLong(), tenantClass);
+    else
+      return create(null, tenantClass);
   }
 
   /**
-   * Creates a new context that has the given taskid, but no tenant class or any parent ids
+   * Creates and returns a new Context.  The context is not set anywhere; call a set(Context)
+   * method to set it to the active context.
+   * @param taskid The task ID to set in the context, or null if no task id desired
+   * @param tenantclass The tenant class to set in the context, or null if no tenant class desired
+   * @param parentIds The causal parent ids to set in the context, optional, and only valid if taskid is not null
+   * @return
    */
-  public static Context newTask(Long taskid, boolean isTaskCausal) {
-    if (isTaskCausal)
-      return create(taskid, null, 0L);
-    else
-      return create(taskid, null);
-  }
-
-  /**
-   * Creates a new context that has the given taskid and tenant class
-   */
-  public static Context newTask(Long taskid, Integer tenantclass, boolean isTaskCausal) {
-    if (isTaskCausal)
-      return create(taskid, tenantclass, 0L);
-    else
-      return create(taskid, tenantclass);
-  }
-  
-  /**
-   * Creates a new context that has the given tenant class, but no taskid
-   */
-  public static Context forTenant(Integer tenantclass) {
-    return create(null, tenantclass);
-  }
-  
   public static Context create(Long taskid, Integer tenantclass, Long... parentIds) {
     Builder builder = XTraceMetadata.newBuilder();
     if (taskid!=null)
       builder.setTaskID(taskid);
     if (tenantclass!=null)
       builder.setTenantClass(tenantclass);
-    for (Long parentid : parentIds)
-      if (parentid!=null)
-        builder.addParentEventID(parentid);
+    if (taskid!=null)
+      for (Long parentid : parentIds)
+        if (parentid!=null)
+          builder.addParentEventID(parentid);
     return new Context(builder);
   }
   
